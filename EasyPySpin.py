f5c3e6090e3c5a84aa6ec43429cf0f4d34b9d9c2
deva2211 <55929613+deva2211@users.noreply.github.com>
2020-03-08 14:12:30 -0700

    Add files via upload

---
+++ EasyPySpin.py
@@ -0,0 +1,649 @@
+import cv2
+
+import PySpin
+
+
+
+class VideoCapture:
+
+    """
+
+    Open a FLIR camera for video capturing.
+
+
+
+    Attributes
+
+    ----------
+
+    cam : PySpin.CameraPtr
+
+        camera
+
+    nodemap : PySpin.INodeMap
+
+        nodemap represents the elements of a camera description file.
+
+
+
+    Methods
+
+    -------
+
+    read()
+
+        returns the next frame.
+
+    release()
+
+        Closes capturing device.
+
+    isOpened()
+
+        Whether a camera is open or not.
+
+    set(propId, value)
+
+        Sets a property.
+
+    get(propId)
+
+        Gets a property.
+
+    """
+
+    def __init__(self, index):
+
+        """
+
+        Parameters
+
+        ----------
+
+        index : int
+
+            id of the video capturing device to open.
+
+        """
+
+        self._system = PySpin.System.GetInstance()
+
+        self._cam_list = self._system.GetCameras()
+
+        #num_cam = self.cam_list.GetSize()
+
+        try:
+
+            if type(index) is int:
+
+                self.cam = self._cam_list.GetByIndex(index)
+
+            else:
+
+                self.cam = self._cam_list.GetBySerial(index)
+
+        except:
+
+            print("camera failed to properly initialize!")
+
+            return None
+
+
+
+        self.cam.Init()
+
+        self.nodemap = self.cam.GetNodeMap()
+
+        
+
+        s_node_map = self.cam.GetTLStreamNodeMap()
+
+        handling_mode = PySpin.CEnumerationPtr(s_node_map.GetNode('StreamBufferHandlingMode'))
+
+        handling_mode_entry = handling_mode.GetEntryByName('NewestOnly')
+
+        handling_mode.SetIntValue(handling_mode_entry.GetValue())
+
+        
+
+    def __del__(self):
+
+        try:
+
+            if self.cam.IsStreaming():
+
+                self.cam.EndAcquisition()
+
+            self.cam.DeInit()
+
+            del self.cam
+
+            self._cam_list.Clear()
+
+            self._system.ReleaseInstance()
+
+        except: pass
+
+
+
+    def release(self):
+
+        """
+
+        Closes capturing device. The method call VideoCapture destructor.
+
+        """
+
+        self.__del__()
+
+
+
+    def isOpened(self):
+
+        """
+
+        Returns true if video capturing has been initialized already.
+
+        """
+
+        try: return self.cam.IsValid()
+
+        except: return False
+
+
+
+    def read(self):
+
+        """
+
+        returns the next frame.
+
+
+
+        Returns
+
+        -------
+
+        retval : bool
+
+            false if no frames has been grabbed.
+
+        image : array_like 
+
+            grabbed image is returned here. If no image has been grabbed the image will be None.
+
+        """
+
+        if not self.cam.IsStreaming():
+
+            self.cam.BeginAcquisition()
+
+
+
+        image = self.cam.GetNextImage()
+
+        if image.IsIncomplete():
+
+            return False, None
+
+        
+
+        img_NDArray = image.GetNDArray()
+
+        image.Release()
+
+        return True, img_NDArray
+
+    
+
+    def set(self, propId, value):
+
+        """
+
+        Sets a property in the VideoCapture.
+
+
+
+        Parameters
+
+        ----------
+
+        propId_id : cv2.VideoCaptureProperties
+
+            Property identifier from cv2.VideoCaptureProperties
+
+        value : int or float or bool
+
+            Value of the property.
+
+        
+
+        Returns
+
+        -------
+
+        retval : bool
+
+           True if property setting success.
+
+        """
+
+        #Exposure setting
+
+        if propId==cv2.CAP_PROP_EXPOSURE:
+
+            #Auto
+
+            if value<0: return self._set_ExposureAuto(PySpin.ExposureAuto_Continuous)
+
+
+
+            #Manual
+
+            ret = self._set_ExposureAuto(PySpin.ExposureAuto_Off)
+
+            if ret==False: return False
+
+            return self._set_ExposureTime(value)
+
+        
+
+        #Gain setting
+
+        if propId==cv2.CAP_PROP_GAIN:
+
+            #Auto
+
+            if value<0: return self._set_GainAuto(PySpin.GainAuto_Continuous)
+
+            
+
+            #Manual
+
+            ret = self._set_GainAuto(PySpin.GainAuto_Off)
+
+            if ret==False: return False
+
+            return self._set_Gain(value)
+
+
+
+        #Brightness(EV) setting
+
+        if propId==cv2.CAP_PROP_BRIGHTNESS:
+
+            return self._set_Brightness(value)
+
+        
+
+        #Gamma setting
+
+        if propId==cv2.CAP_PROP_GAMMA:
+
+            return self._set_Gamma(value)
+
+
+
+        #FrameRate setting
+
+        if propId==cv2.CAP_PROP_FPS:
+
+            return self._set_FrameRate(value)
+
+
+
+        #BackLigth setting
+
+        if propId==cv2.CAP_PROP_BACKLIGHT:
+
+            return self._set_BackLight(value)
+
+
+
+        return False
+
+    
+
+    def get(self, propId):
+
+        """
+
+        Returns the specified VideoCapture property.
+
+        
+
+        Parameters
+
+        ----------
+
+        propId_id : cv2.VideoCaptureProperties
+
+            Property identifier from cv2.VideoCaptureProperties
+
+        
+
+        Returns
+
+        -------
+
+        value : int or float or bool
+
+           Value for the specified property. Value Flase is returned when querying a property that is not supported.
+
+        """
+
+        if propId==cv2.CAP_PROP_EXPOSURE:
+
+            return self._get_ExposureTime()
+
+
+
+        if propId==cv2.CAP_PROP_GAIN:
+
+            return self._get_Gain()
+
+
+
+        if propId==cv2.CAP_PROP_BRIGHTNESS:
+
+            return self._get_Brightness()
+
+
+
+        if propId==cv2.CAP_PROP_GAMMA:
+
+            return self._get_Gamma()
+
+
+
+        if propId==cv2.CAP_PROP_FRAME_WIDTH:
+
+            return self._get_Width()
+
+
+
+        if propId==cv2.CAP_PROP_FRAME_HEIGHT:
+
+            return self._get_Height()
+
+
+
+        if propId==cv2.CAP_PROP_FPS:
+
+            return self._get_FrameRate()
+
+
+
+        if propId==cv2.CAP_PROP_TEMPERATURE:
+
+            return self._get_Temperature()
+
+
+
+        if propId==cv2.CAP_PROP_BACKLIGHT:
+
+            return self._get_BackLight()
+
+
+
+        return False
+
+    
+
+    def __clip(self, a, a_min, a_max):
+
+        return min(max(a, a_min), a_max)
+
+    
+
+    def _set_ExposureTime(self, value):
+
+        if not type(value) in (int, float): return False
+
+        exposureTime_to_set = self.__clip(value, self.cam.ExposureTime.GetMin(), self.cam.ExposureTime.GetMax())
+
+        self.cam.ExposureTime.SetValue(exposureTime_to_set)
+
+        return True
+
+
+
+    def _set_ExposureAuto(self, value):
+
+        self.cam.ExposureAuto.SetValue(value)
+
+        return True
+
+
+
+    def _set_Gain(self, value):
+
+        if not type(value) in (int, float): return False
+
+        gain_to_set = self.__clip(value, self.cam.Gain.GetMin(), self.cam.Gain.GetMax())
+
+        self.cam.Gain.SetValue(gain_to_set)
+
+        return True
+
+
+
+    def _set_GainAuto(self, value):
+
+        self.cam.GainAuto.SetValue(value)
+
+        return True
+
+    
+
+    def _set_Brightness(self, value):
+
+        if not type(value) in (int, float): return False
+
+        brightness_to_set = self.__clip(value, self.cam.AutoExposureEVCompensation.GetMin(), self.cam.AutoExposureEVCompensation.GetMax())
+
+        self.cam.AutoExposureEVCompensation.SetValue(brightness_to_set)
+
+        return True
+
+
+
+    def _set_Gamma(self, value):
+
+        if not type(value) in (int, float): return False
+
+        gamma_to_set = self.__clip(value, self.cam.Gamma.GetMin(), self.cam.Gamma.GetMax())
+
+        self.cam.Gamma.SetValue(gamma_to_set)
+
+        return True
+
+
+
+    def _set_FrameRate(self, value):
+
+        if not type(value) in (int, float): return False
+
+        self.cam.AcquisitionFrameRateEnable.SetValue(True)
+
+        fps_to_set = self.__clip(value, self.cam.AcquisitionFrameRate.GetMin(), self.cam.AcquisitionFrameRate.GetMax())
+
+        self.cam.AcquisitionFrameRate.SetValue(fps_to_set)
+
+        return True
+
+
+
+    def _set_BackLight(self, value):
+
+        if value==True:backlight_to_set = PySpin.DeviceIndicatorMode_Active
+
+        elif value==False: backlight_to_set = PySpin.DeviceIndicatorMode_Inactive
+
+        else: return False
+
+        self.cam.DeviceIndicatorMode.SetValue(backlight_to_set)
+
+        return True
+
+
+
+    def _get_ExposureTime(self):
+
+        return self.cam.ExposureTime.GetValue()
+
+
+
+    def _get_Gain(self):
+
+        return self.cam.Gain.GetValue()
+
+
+
+    def _get_Brightness(self):
+
+        return self.cam.AutoExposureEVCompensation.GetValue()
+
+
+
+    def _get_Gamma(self):
+
+        return self.cam.Gamma.GetValue()
+
+
+
+    def _get_Width(self):
+
+        return self.cam.Width.GetValue()
+
+
+
+    def _get_Height(self):
+
+        return self.cam.Height.GetValue()
+
+
+
+    def _get_FrameRate(self):
+
+        return self.cam.AcquisitionFrameRate.GetValue()
+
+
+
+    def _get_Temperature(self):
+
+        return self.cam.DeviceTemperature.GetValue()
+
+
+
+    def _get_BackLight(self):
+
+        status = self.cam.DeviceIndicatorMode.GetValue()
+
+        return (True  if status == PySpin.DeviceIndicatorMode_Active else
+
+                False if status == PySpin.DeviceIndicatorMode_Inactive else
+
+                status)
+
+
+
+def main():
+
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-i", "--index", type=int, default=0, help="Camera index (Default: 0)")
+
+    parser.add_argument("-e", "--exposure",type=float, default=-1, help="Exposure time [us] (Default: Auto)")
+
+    parser.add_argument("-g", "--gain", type=float, default=-1, help="Gain [dB] (Default: Auto)")
+
+    parser.add_argument("-G", "--gamma", type=float, help="Gamma value")
+
+    parser.add_argument("-b", "--brightness", type=float, help="Brightness [EV]")
+
+    parser.add_argument("-f", "--fps", type=float, help="FrameRate [fps]")
+
+    parser.add_argument("-s", "--scale", type=float, default=0.25, help="Image scale to show (>0) (Default: 0.25)")
+
+    args = parser.parse_args()
+
+
+
+    cap = VideoCapture(args.index)
+
+
+
+    if not cap.isOpened():
+
+        print("Camera can't open\nexit")
+
+        return -1
+
+    
+
+    cap.set(cv2.CAP_PROP_EXPOSURE, args.exposure) #-1 sets exposure_time to auto
+
+    cap.set(cv2.CAP_PROP_GAIN, args.gain) #-1 sets gain to auto
+
+    if args.gamma      is not None: cap.set(cv2.CAP_PROP_GAMMA, args.gamma)
+
+    if args.fps        is not None: cap.set(cv2.CAP_PROP_FPS, args.fps)
+
+    if args.brightness is not None: cap.set(cv2.CAP_PROP_BRIGHTNESS, args.brightness)
+
+
+
+    while True:
+
+        ret, frame = cap.read()
+
+
+
+        exposureTime = cap.get(cv2.CAP_PROP_EXPOSURE)
+
+        gain = cap.get(cv2.CAP_PROP_GAIN)
+
+        print("exposureTime:", exposureTime)
+
+        print("gain        :", gain)
+
+        print("\033[2A",end="")
+
+        
+
+        img_show = cv2.resize(frame, None, fx=args.scale, fy=args.scale)
+
+        cv2.imshow("capture", img_show)
+
+        key = cv2.waitKey(30)
+
+        if key==ord("q"):
+
+            break
+
+        elif key==ord("c"):
+
+            import datetime
+
+            time_stamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+
+            filepath = time_stamp + ".png"
+
+            cv2.imwrite(filepath, frame)
+
+            print("Export > ", filepath)
+
+    
+
+    cv2.destroyAllWindows()
+
+    cap.release()
+
+
+
+if __name__=="__main__":
+
+    main()
---
+++ imreaderV3.1.py
@@ -0,0 +1,152 @@
+# USAGE
+# python barcode_scanner_video.py
+
+# import the necessary packages
+from imutils.video import VideoStream
+from pyzbar import pyzbar
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+import EasyPySpin
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+#An arguement to specify the particular csv file
+ap.add_argument("-o", "--output", type=str, default="barcodes.csv",
+	help="path to output CSV file containing barcodes")
+#An arguement to specify the particular image we are looking for; this one is necessary
+ap.add_argument("-i", "--ref", required=True,
+	help="path to input image")
+#Dictionary with all command line inputted values
+args = vars(ap.parse_args())
+
+
+
+
+
+
+
+#Pull out the string that shows what image we are looking for
+reference = args["ref"]
+print ("The image we are looking for: ")
+print(reference)
+
+
+
+
+
+
+
+#load the binary of the image that corresponds to the string that we put in, taking this info from the saved image
+binary = cv2.imread(args["ref"])
+
+
+
+
+
+#Extrapolate the relevant values from the binary 
+barcode_we_want = pyzbar.decode(binary)
+
+
+
+
+
+#Using those values flat out decode the barcodes in the reference image to string.
+for barcode in barcode_we_want:
+	barcode_we_want_string = barcode.data.decode("utf-8")
+
+print("The string we are looking for: ")
+print(barcode_we_want_string)
+	
+
+
+
+
+# initialize the video stream and allow the camera sensor to warm up
+print("Starting video stream...")
+vs = EasyPySpin.VideoCapture(0)
+time.sleep(2.0)
+
+# open the output CSV file for writing 
+#create an empty iterating variable to hold the set of strings found
+csv = open(args["output"], "w")
+found = set()
+
+
+
+
+
+
+
+
+
+
+
+# loop over the frames from the video stream
+while True:
+	# grab the frame from the threaded video stream and resize it to
+	# have a maximum width of 400 pixels
+	(grabbed,frame) = vs.read()
+	frame = imutils.resize(frame, width=400)
+
+	# find the barcodes in the frame and decode each of the barcodes
+	barcodes_in_frame = pyzbar.decode(frame)
+
+#loop over the detected barcodes
+	for barcode in barcodes_in_frame:
+
+		# the barcode data is a bytes object so if we want to draw it
+		# on our output image we need to convert it to a string first
+		barcodeData = barcode.data.decode("utf-8")
+		barcodeType = barcode.type
+
+		# extract the bounding box location of the barcode and draw
+		# the bounding box surrounding the barcode on the image
+		(x, y, w, h) = barcode.rect
+
+		#If the barcode string matches draw the box in green, if not, red
+		if barcode_we_want_string == barcodeData:
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+		else:
+			cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)	
+
+		# draw the barcode data and barcode type on the image
+		text = "{} ({})".format(barcodeData, barcodeType)
+
+		#If the barcode string matches output text in green, if not, red
+		if barcode_we_want_string == barcodeData:
+			cv2.putText(frame, text, (x, y - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		else:
+			cv2.putText(frame, text, (x, y - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+		
+
+		# if the barcode text is currently not in our CSV file, write
+		# the timestamp + barcode to disk and update the set
+		if barcodeData not in found:
+			csv.write("{},{}\n".format(datetime.datetime.now(),
+			barcodeData))
+			csv.flush()
+			found.add(barcodeData)
+
+	# show the output frame
+	cv2.imshow("Barcode Scanner", frame)
+	key = cv2.waitKey(1) & 0xFF
+ 
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+
+
+
+
+
+
+# close the output CSV file do a bit of cleanup
+print("[INFO] cleaning up...")
+csv.close()
+cv2.destroyAllWindows()
